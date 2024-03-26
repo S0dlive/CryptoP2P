384d8fa20bbbe77cf2d02175dc0a77ac73c17421
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-03-26 23:26:19 +0100

    feat(block-chain): chains.

--- src/CryptoP2P.Network/BlockChain/BlockChain.cs
+++ src/CryptoP2P.Network/BlockChain/BlockChain.cs
@@ -1,17 +1,33 @@
+using Microsoft.Extensions.Logging;
 namespace CryptoP2P.Network.BlockChain;
 
-public static class BlockChain
+public class BlockChain
 {
-    public static DateTime LastUpdate { get; set; } 
-    public static List<Chain> Chains { get; set; } = new List<Chain>();
-
-    public static async Task UpdateLocalBlockChain()
+    private readonly ILogger _logger;
+    public BlockChain()
+    {
+        ILoggerFactory factory = LoggerFactory
+            .Create(builder => builder.AddConsole());
+        _logger = factory.CreateLogger("BlockChain");
+        InitializeBlockChainAsync();
+    }
+    public DateTime LastUpdate { get; set; } 
+    public List<Chain> Chains { get; set; } 
+    
+    public async Task UpdateLocalBlockChainAsync()
     {
         LastUpdate = DateTime.UtcNow;
         throw new NotImplementedException();
     }
 
-    private static void AddChains(Chain chain)
+    private async Task InitializeBlockChainAsync()
+    {
+        _logger.LogInformation("Blockchain initialization is underway. . .");
+        Chains = new List<Chain>();
+        // TODO: Request with P2P network and implementation
+        _logger.LogInformation("Blockchain initialization is finish.");
+    }
+    private void AddChains(Chain chain)
     {
         throw new NotImplementedException();
     }
--- src/CryptoP2P.Network/BlockChain/Chain.cs
+++ src/CryptoP2P.Network/BlockChain/Chain.cs
@@ -1,6 +1,30 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace CryptoP2P.Network.BlockChain;
 
 public abstract class Chain
 {
+    public string Hash { get; set; }
+    public string PreviousHash { get; set; }
+    public virtual List<object> Data { get; set; }
     
+    protected string GenerateRandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    public string GenerateHash()
+    {
+        var text = GenerateRandomString(32);
+        byte[] hash;
+        using (SHA512 sha512 = SHA512.Create())
+        {
+            hash = sha512.ComputeHash(Encoding.UTF8.GetBytes(text));
+        }
+        return hash.ToString();
+    }
 }
