b5226419e8c3c5a97e53cee4082d2f0e82521479
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-03-26 20:43:36 +0100

    feat(test): UnitTest.

---
+++ src/CryptoP2P.Network/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+public class Program
+{
+    public static void Main()
+    {
+        ILoggerFactory factory = LoggerFactory
+            .Create(builder => builder.AddConsole());
+        
+        ILogger logger = factory.CreateLogger("Start");
+        logger.LogInformation("start of the connection with the crypto network. . .");
+    }
+}
---
+++ test/CryptoP2P.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace CryptoP2P.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
