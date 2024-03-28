b02683a3be2db85e2315c663b9922276650aaace
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-03-28 20:03:38 +0100

    feat(client) client some implementation.

--- src/CryptoP2P.Node/P2P/Client.cs
+++ src/CryptoP2P.Node/P2P/Client.cs
@@ -8,5 +8,38 @@ public class Client
     public string ClientId { get; set; }
     public TcpClient TCPClient { get; set; }
     public string ClientName { get; set; }
+    public NetworkStream Stream { get; set; }
     public DateTime ConnectedSince { get; set; }
+    private byte[] receivedBuffer { get; set; }
+
+    public async Task ConnectWithServer(TcpClient client)
+    {
+        TCPClient.ReceiveBufferSize = 4096;
+        TCPClient.SendBufferSize = 4096;
+        Stream = client.GetStream();
+
+        receivedBuffer = new byte[4096];
+        Stream.BeginRead(receivedBuffer, 0, 4096, ReceivedCallback, null);
+    }
+
+    private void ReceivedCallback(IAsyncResult ar)
+    {
+        try
+        {
+            int byteLenght = Stream.EndRead(ar);
+            if (byteLenght <= 0 )
+            {
+                // TODO: Disconnet 
+                return;
+            }
+
+            byte[] data = new byte[4096];
+            Stream.BeginRead(receivedBuffer, 0, 4096, ReceivedCallback, null);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
