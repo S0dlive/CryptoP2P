68a7a8ac525292c7c64d662a4e70416c0af4df4e
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-03-28 20:04:59 +0100

    feat(program): program

--- src/CryptoP2P.Node/Program.cs
+++ src/CryptoP2P.Node/Program.cs
@@ -1,15 +1,16 @@
 ﻿using CryptoP2P.Network.BlockChain;
+using CryptoP2P.Network.Node;
 using Microsoft.Extensions.Logging;
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         ILoggerFactory factory = LoggerFactory
             .Create(builder => builder.AddConsole());
-        
         ILogger logger = factory.CreateLogger("Start");
         logger.LogInformation("start of the connection with the crypto network. . .");
-        
+        Node.StartNode();
+        Console.ReadKey();
     }
 }
