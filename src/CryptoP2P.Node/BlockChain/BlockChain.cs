27f97bc54a69f154e60045df51bb57abd12fd5e0
Baptiste <92639070+S0dlive@users.noreply.github.com>
2024-03-30 21:05:53 +0100

    With Block Commit. TODO: RESTRUCTURE BlockChain.cs.

--- src/CryptoP2P.Node/BlockChain/BlockChain.cs
+++ src/CryptoP2P.Node/BlockChain/BlockChain.cs
@@ -1,36 +1,28 @@
+using System.Collections;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Logging;
 namespace CryptoP2P.Network.BlockChain;
 
 public class BlockChain
 {
-    [JsonIgnore]
     private readonly ILogger _logger;
+    private List<Block> Blocks { get; set; }
     public BlockChain()
     {
+        Blocks = new List<Block>();
         ILoggerFactory factory = LoggerFactory
             .Create(builder => builder.AddConsole());
-        _logger = factory.CreateLogger("BlockChain");
-        InitializeBlockChainAsync();
-    }
-    public DateTime LastUpdate { get; set; } 
-    public List<Chain> Chains { get; set; } 
-    
-    public async Task UpdateLocalBlockChainAsync()
-    {
-        LastUpdate = DateTime.UtcNow;
-        throw new NotImplementedException();
+        _logger = factory.CreateLogger("Node");
     }
 
-    private async Task InitializeBlockChainAsync()
+    public void AddBlock(Block block)
     {
-        _logger.LogInformation("Blockchain initialization is underway. . .");
-        Chains = new List<Chain>();
-        // TODO: Request with P2P network and implementation
-        _logger.LogInformation("Blockchain initialization is finish.");
-    }
-    private void AddChains(Chain chain)
-    {
-        throw new NotImplementedException();
+        if (Blocks.Last().Hash != block.PreviousHash)
+        {
+            _logger.LogError("not good previousHash.");
+            return;
+        }
+        
+        
     }
 }
